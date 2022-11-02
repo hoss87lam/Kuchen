a83542bc4a92c9f4b12bd450a676a4bd8904f750
Holger L <115541826+hoss87lam@users.noreply.github.com>
2022-11-02 10:23:17 +0100

    Ursprungsversion von Kuchen.java

---
+++ data/Kuchen.java
@@ -0,0 +1,14 @@
+package data;
+
+/**
+ * Kuchen mit name, preis, und gewicht
+ * Anzeige des Kuchen nur mit Namen und Preis
+ * 
+ * @author Holger L.
+ *
+ */
+public class Kuchen {
+	private String name;
+	private double preis;
+	private double gewicht;
+}
